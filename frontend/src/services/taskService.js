dbe9a10a861d9e40700587c4c1c2957a64e0b3f4
Hemant Ahire <hemantahire002@gmail.com>
2025-02-25 17:29:16 +0530

    kazam new project

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://kazamnewprojectbackend.onrender.com/api/tasks";
 
 export const fetchTasks = async () => {
     try {
